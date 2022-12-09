8d1cc0c0d5adac188019060c9045f02b3c51a09e
yosidrip <eyosimina2@gmail.com>
2022-12-09 01:29:57 -0800

    Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entery
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeo f(char));
+	printf("Size of a char: %i byte(s)\n", sizeo f(int));
+	printf("Size of a char: %i byte(s)\n", sizeo f(long int));
+	printf("Size of a char: %i byte(s)\n", sizeo f(long long int));
+	printf("Size of a char: %i byte(s)\n", sizeo f(float));
+	return (0);
+}
