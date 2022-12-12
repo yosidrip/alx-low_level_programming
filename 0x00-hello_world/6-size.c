c4dbb04873b55725aa8972c6b081864d39812221
yosidrip <eyosimina2@gmail.com>
2022-12-12 07:29:52 -0800

    Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	printf("Size of a char: %i byte(s)\n", sizeof(a));
 	printf("Size of a int: %i byte(s)\n", sizeof(b));
 	printf("Size of a long int: %i byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %i byte(s)\n" sizeof(d));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(d));
 	printf("Size of a float: %i byte(s)\n", sizeof(f));
 	return (0);
 }
