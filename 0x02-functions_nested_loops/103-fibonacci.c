630fe5ce79c0bce2264f58dd8562a85186a839ab
yosidrip <eyosimina2@gmail.com>
2022-12-15 01:30:35 -0800

    Functions, nested loops

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -4,19 +4,19 @@
  * less than 4000000.
  * Return: nothing!
  */
-int main (void)
+int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2, sum = k;
+	int a = 0, b = 1, n = 0;
+	int sum = 0;
 
-	while (k + j < 4000000)
+	while (n < 4000000)
 	{
-		k += j;
-		if (k % 2 == 0)
-			sum += k;
-		j = k - j;
-		++i;
+		n = a + b;
+		a = b;
+		b = n;
+		if (next % 2 == 0)
+			sum += n;
 	}
-	printf("%ld\n", sum);
+	print("%i\n", sum);
 	return (0);
 }
