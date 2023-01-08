fb12cc3776b590f10b0567f029200de353c7b811
yosidrip <eyosimina2@gmail.com>
2023-01-08 10:25:31 -0800

    argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,7 +8,7 @@
  * @argv: This is the argument vector
  * Return: 0;
  */
-int main(int argc, char *argv [])
+int main(int argc, char *argv[])
 {
 	int sum = 0, i;
 
@@ -18,7 +18,7 @@ int main(int argc, char *argv [])
 		{
 			int b;
 			char *str;
-			
+
 			str = argv[i];
 			for (b = 0; str[b] != '\0'; b++)
 			{
@@ -32,7 +32,7 @@ int main(int argc, char *argv [])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		sum += atoi (argv[i]);
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
